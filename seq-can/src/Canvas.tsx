eedd4354723436686e318a924a79c5f5c902fd91
Mike Deem <mike@eyeree.me>
2022-01-30 21:34:19 -0800

    got rid of canvas container element

--- seq-can/src/Canvas.tsx
+++ seq-can/src/Canvas.tsx
@@ -5,15 +5,12 @@ import createjs from 'eyeree-createjs-module';
 export type CanvasProps = {}
 
 const Canvas = (props:CanvasProps) => {
-  
-  const canvasRef = useRef<HTMLCanvasElement>(null)
 
   const resizeObserver = React.useRef<ResizeObserver>(new ResizeObserver((entries:ResizeObserverEntry[]) => {
     
-    if(canvasRef.current == null) return;
+    const canvas = entries[0].target as HTMLCanvasElement;
 
-    const canvas = canvasRef.current;
-    const { width, height } = canvas.getBoundingClientRect()
+    const { width, height } = canvas.getBoundingClientRect();
 
     if (canvas.width !== width || canvas.height !== height) {
       const { devicePixelRatio:ratio=1 } = window
@@ -26,39 +23,40 @@ const Canvas = (props:CanvasProps) => {
     
   }));
 
-  const resizedContainerRef = React.useCallback((container: HTMLDivElement) => {    
-    if (container !== null) {
-      resizeObserver.current.observe(container);
+  const canvasRef = React.useCallback((canvas: HTMLCanvasElement) => {    
+    if (canvas !== null) {
+
+      resizeObserver.current.observe(canvas);
+
+      var stage = new createjs.Stage(canvas);
+      var circle = new createjs.Shape();
+      circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+      circle.x = 50;
+      circle.y = 50;
+      stage.addChild(circle);
+    
+      stage.update();
+    
+      createjs.Tween.get(circle, { loop: true })
+      .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+      .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
+      .to({ alpha: 0, y: 225 }, 100)
+      .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
+      .to({ x: 50 }, 800, createjs.Ease.getPowInOut(2))
+      .to({ y: 50 }, 2000, createjs.Ease.bounceIn)
+    
+      createjs.Ticker.framerate = 60;
+      createjs.Ticker.addEventListener("tick", stage);
+  
     } else {
       if (resizeObserver.current)
         resizeObserver.current.disconnect();
+        createjs.Ticker.removeAllEventListeners();
       }
   }, [resizeObserver.current]);
 
-  useEffect(() => {
-    if(canvasRef.current == null) return;
-    var stage = new createjs.Stage(canvasRef.current);
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 50;
-    circle.y = 50;
-    stage.addChild(circle);
-  
-    stage.update();
-  
-    createjs.Tween.get(circle, { loop: true })
-    .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-    .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
-    .to({ alpha: 0, y: 225 }, 100)
-    .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-    .to({ x: 50 }, 800, createjs.Ease.getPowInOut(2))
-    .to({ y: 50 }, 2000, createjs.Ease.bounceIn)
-  
-    createjs.Ticker.framerate = 60;
-    createjs.Ticker.addEventListener("tick", stage);
-  }, [])
-  
-  return <div className="CanvasContainer" ref={resizedContainerRef}><canvas ref={canvasRef} {...props}/></div>
+  return <canvas ref={canvasRef} {...props}/>;
+
 }
 
 export default Canvas
