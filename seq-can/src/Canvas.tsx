eb4a6a3a3022f072217cfb0371c48fe250c6c9ca
Mike Deem <mike@eyeree.me>
2022-01-30 21:14:28 -0800

    drawing with createjs

---
+++ seq-can/src/Canvas.tsx
@@ -0,0 +1,65 @@
+import React, { useRef, useEffect } from 'react'
+import './Canvas.css';
+import createjs from 'eyeree-createjs-module';
+
+export type CanvasProps = {}
+
+const Canvas = (props:CanvasProps) => {
+  
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  const resizeObserver = React.useRef<ResizeObserver>(new ResizeObserver((entries:ResizeObserverEntry[]) => {
+    
+    if(canvasRef.current == null) return;
+
+    const canvas = canvasRef.current;
+    const { width, height } = canvas.getBoundingClientRect()
+
+    if (canvas.width !== width || canvas.height !== height) {
+      const { devicePixelRatio:ratio=1 } = window
+      const context = canvas.getContext('2d')!
+      canvas.width = width*ratio
+      canvas.height = height*ratio
+      context.scale(ratio, ratio)
+      return true
+    }
+    
+  }));
+
+  const resizedContainerRef = React.useCallback((container: HTMLDivElement) => {    
+    if (container !== null) {
+      resizeObserver.current.observe(container);
+    } else {
+      if (resizeObserver.current)
+        resizeObserver.current.disconnect();
+      }
+  }, [resizeObserver.current]);
+
+  useEffect(() => {
+    if(canvasRef.current == null) return;
+    var stage = new createjs.Stage(canvasRef.current);
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 50;
+    circle.y = 50;
+    stage.addChild(circle);
+  
+    stage.update();
+  
+    createjs.Tween.get(circle, { loop: true })
+    .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+    .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
+    .to({ alpha: 0, y: 225 }, 100)
+    .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
+    .to({ x: 50 }, 800, createjs.Ease.getPowInOut(2))
+    .to({ y: 50 }, 2000, createjs.Ease.bounceIn)
+  
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.addEventListener("tick", stage);
+  }, [])
+  
+  return <div className="CanvasContainer" ref={resizedContainerRef}><canvas ref={canvasRef} {...props}/></div>
+}
+
+export default Canvas
+
